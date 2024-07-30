8121c8327c2fb8633af89332711c91ccc163c240
trinhtuanvubk <vutuantrinh2000@gmail.com>
2024-07-30 09:03:48 +0000

    update more args

--- download.sh
+++ download.sh
@@ -2,7 +2,18 @@
 
 if [ ! -f uvr5_weights/2_HP-UVR.pth ]; then
   echo "Download the model weights"
-  wget -q -O uvr5_weights/2_HP-UVR.pth 2_HP-UVR.pth https://huggingface.co/fastrolling/uvr/resolve/main/Main_Models/2_HP-UVR.pth 
+  wget -q -O uvr5_weights/2_HP-UVR.pth 2_HP-UVR.pth https://huggingface.co/fastrolling/uvr/resolve/main/Main_Models/2_HP-UVR.pth
+fi
+
+if [ ! -f uvr5_weights/3_HP-Vocal-UVR.pth ]; then
+  echo "Download the model weights"
+  wget -q -O uvr5_weights/3_HP-Vocal-UVR.pth 3_HP-UVR.pth https://huggingface.co/fastrolling/uvr/resolve/main/Main_Models/3_HP-Vocal-UVR.pth
+fi
+
+
+if [ ! -f uvr5_weights/4_HP-Vocal-UVR.pth ]; then
+  echo "Download the model weights"
+  wget -q -O uvr5_weights/4_HP-Vocal-UVR.pth 4_HP-UVR.pth https://huggingface.co/fastrolling/uvr/resolve/main/Main_Models/4_HP-Vocal-UVR.pth
 fi
 
 echo "The model weights have been downloaded"
--- separate.py
+++ separate.py
@@ -13,6 +13,7 @@ from scipy.io import wavfile
 class  _audio_pre_():
     def __init__(self, model_path,device,is_half):
         self.model_path = model_path
+        self.model_name = model_path.split("/")[-1].rsplit(".")[0]
         self.device = device
         self.data = {
             # Processing Options
@@ -25,13 +26,17 @@ class  _audio_pre_():
         }
         nn_arch_sizes = [
             31191, # default
-            33966,61968, 123821, 123812, 537238 # custom
+            33966, 56817, 123821, 123812, 129605, 218409, 537238, 537227
         ]
         self.nn_architecture = list('{}KB'.format(s) for s in nn_arch_sizes)
         model_size = math.ceil(os.stat(model_path ).st_size / 1024)
+        # print(f"model_size: {model_size}")
         nn_architecture = '{}KB'.format(min(nn_arch_sizes, key=lambda x:abs(x-model_size)))
+        
+        # print(f"nn_arch: {nn_architecture}")
         nets = importlib.import_module('uvr5_pack.lib_v5.nets' + f'_{nn_architecture}'.replace('_{}KB'.format(nn_arch_sizes[0]), ''), package=None)
         model_hash = hashlib.md5(open(model_path,'rb').read()).hexdigest()
+        # print(f"model_hash: {model_hash}")
         param_name ,model_params_d = _get_name_params(model_path , model_hash)
 
         mp = ModelParameters(model_params_d)
@@ -88,7 +93,7 @@ class  _audio_pre_():
             else:
                 wav_instrument = spec_utils.cmb_spectrogram_to_wave(y_spec_m, self.mp)
             print ('%s instruments done'%name)
-            wavfile.write(os.path.join(ins_root, 'instrument_{}.wav'.format(name) ), self.mp.param['sr'], (np.array(wav_instrument)*32768).astype("int16"))  #
+            wavfile.write(os.path.join(ins_root, 'instrument_{}_{}'.format(self.model_name, name)), self.mp.param['sr'], (np.array(wav_instrument)*32768).astype("int16"))  #
         if (vocal_root is not None):
             if self.data['high_end_process'].startswith('mirroring'):
                 input_high_end_ = spec_utils.mirroring(self.data['high_end_process'],  v_spec_m, input_high_end, self.mp)
@@ -96,13 +101,37 @@ class  _audio_pre_():
             else:
                 wav_vocals = spec_utils.cmb_spectrogram_to_wave(v_spec_m, self.mp)
             print ('%s vocals done'%name)
-            wavfile.write(os.path.join(vocal_root , 'vocal_{}.wav'.format(name) ), self.mp.param['sr'], (np.array(wav_vocals)*32768).astype("int16"))
+            wavfile.write(os.path.join(vocal_root , 'vocal_{}_{}'.format(self.model_name, name)), self.mp.param['sr'], (np.array(wav_vocals)*32768).astype("int16"))
 
-if __name__ == '__main__':
-    device = 'cuda'
-    is_half=True
-    model_path='uvr5_weights/2_HP-UVR.pth'
+def run(model_path, device, is_half, audio_path, save_path, only_save_vocal=True):
     pre_fun = _audio_pre_(model_path=model_path,device=device,is_half=True)
-    audio_path = '9OvlclzngLY.wav'
-    save_path = 'opt'
-    pre_fun._path_audio_(audio_path , save_path,save_path)
+    if audio_path.endswith(".wav"):
+        if only_save_vocal:
+            pre_fun._path_audio_(audio_path, None, save_path)
+        else:
+            pre_fun._path_audio_(audio_path, save_path, save_path)
+    else:
+        # this is folder
+        for i, file in enumerate(os.listdir(audio_path)):
+            print(f"processing {i} - {file}")
+            file_path = os.path.join(audio_path, file)
+            if only_save_vocal:
+                pre_fun._path_audio_(file_path, None, save_path)
+            else:
+                pre_fun._path_audio_(file_path, save_path, save_path)
+            
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default="cuda")
+    parser.add_argument('--is_half', action="store_false")
+    parser.add_argument('--model_path', type=str, default="uvr5_weights/2_HP-UVR.pth")
+    parser.add_argument('--audio_path', type=str, default="9OvlclzngLY.wav")
+    parser.add_argument('--save_path', type=str, default="opt")
+    parser.add_argument('--only_save_vocal', action="store_true")
+
+    args = parser.parse_args()
+    
+    run(args.model_path, args.device, args.is_half, args.audio_path, args.save_path, only_save_vocal=args.only_save_vocal)
+
+    # Example: python3 separate.py --model_path "uvr5_weights/2_HP-UVR.pth" --audio_path 9OvlclzngLY.wav --only_save_vocal
